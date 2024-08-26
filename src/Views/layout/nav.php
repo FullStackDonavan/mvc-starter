abafe473dccdd307d86100d3c18b19b85db33bf8
DonavanJones <donavancjones@gmail.com>
2024-08-26 00:38:34 -0400

    updated name

--- src/Views/layout/nav.php
+++ src/Views/layout/nav.php
@@ -3,7 +3,7 @@
     <!-- Navigation Bar -->
     <header class="bg-gray-800 text-white">
         <nav class="container mx-auto flex items-center justify-between p-4 relative">
-            <a href="/" class="text-2xl font-bold">MyWebsite</a>
+            <a href="/" class="text-2xl font-bold">Starter MVC Framework</a>
             <button id="menu-button" class="block lg:hidden text-gray-300 hover:text-white focus:outline-none z-10">
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
